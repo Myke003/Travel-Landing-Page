b27f58d74a37b871337502fd39bfe8818c168d17
Miguee003 <miguellozano81@outlook.com>
2024-10-07 13:15:59 -0500

    Finalizado

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 
+const menuLinks = document.querySelectorAll('.nav-menu a');
+
+
+// Menú navegación
+menuLinks.forEach(link =>{
+    link.addEventListener('click', function(e){
+        e.preventDefault;
+
+    const targetClass = this.getAttribute('data-target');
+
+    const targetSection = document.querySelector(`.${targetClass}`);
+
+    targetSection.scrollIntoView({
+        behavior: 'smooth'
+      });
+    })
+})
+
+// Menú scroll color
+
+window.addEventListener("scroll", function(){
+    const header = this.document.querySelector("header");
+    header.classList.toggle("down", window.scrollY > 180);
+})
